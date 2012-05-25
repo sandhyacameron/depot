d6590cf2d610aa078f997b099bad5a278b63b982
sandhyacameron <v4sandhya@yahoo.com>
2012-05-24 20:06:48 -0700

    test code

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -2,8 +2,14 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
-    @product = products(:one)
-  end
+   @product = products(:one)
+   @update = {
+    title: 'Lorem Ipsum',
+    description: 'Wibbles are fun!',
+    image_url: 'lorem.jpg',
+    price: 19.95
+   }
+end
 
   test "should get index" do
     get :index
@@ -18,8 +24,8 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
-    end
+    post :create, product: @update
+  end
 
     assert_redirected_to product_path(assigns(:product))
   end
@@ -35,15 +41,14 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+   put :update, id: @product, product: @update
+   assert_redirected_to product_path(assigns(:product)) 
+  end
+#
+  test "should destroy product" do
+    put :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete :destroy, id: @product
-    end
-
-    assert_redirected_to products_path
-  end
+   
 end
