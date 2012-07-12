ea57b334774c42d2eb0efcd0788c00a8287e2321
sandhyacameron <v4sandhya@yahoo.com>
2012-07-11 22:23:38 -0700

    ajax_script

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class LineItemsController < ApplicationController
   # GET /line_items
   # GET /line_items.json
@@ -46,14 +54,13 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, 
-        notice: 'Line item was successfully created.' }
-        format.json { render json: @line_item, 
-        status: :created, location: @line_item }
+        format.html { redirect_to store_url }
+        format.json { render json: @line_item,
+          status: :created, location: @line_item }
       else
         format.html { render action: "new" }
-        format.json { render json: @line_item.errors,    
-                      status: :unprocessable_entity }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
       end
     end
   end
@@ -69,7 +76,7 @@ class LineItemsController < ApplicationController
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @line_item.errors,  status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -86,3 +93,4 @@ class LineItemsController < ApplicationController
     end
   end
 end
+
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,7 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart, :product_id, :product, :quantity
   belongs_to :product
   belongs_to :cart
+  def total_price
+    product.price * quantity 
+  end
 end
